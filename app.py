223813239ecbc99277d3711c36ca0de6a5888673
DIPEN PATEL <kinpat3@yahoo.com>
2019-11-14 14:46:27 -0600

    Grocery_Store

---
+++ app.py
@@ -0,0 +1,5 @@
+from grocerystore import app
+import grocerystore.routes
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ grocerystore/routes.py
@@ -0,0 +1,31 @@
+from flask import render_template, redirect, url_for
+from grocerystore import app
+from grocerystore.models import db
+from grocerystore.forms import ContactForm
+from grocerystore.models import User
+
+
+@app.route("/")
+def hello_world():
+    return render_template("base.html")
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def SendMessage():
+    form = ContactForm()
+    if form.validate_on_submit():
+        print("Your message about {} has been sent".format(form.subject.data))
+        user = User(form.name.data, form.email.data,
+                    form.subject.data, form.message.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("hello_world"))
+    else:
+        print("Message Has Not Been Sent")
+        print(form.errors)
+    return render_template("contact.html", form=form)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
